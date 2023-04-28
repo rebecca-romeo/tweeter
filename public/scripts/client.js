ccad78700087cbf61a38fe6eccb6255c4c5f0b9b
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-27 21:47:30 -0400

    define a function createTweetElement that takes in a tweet object and returns the entire article HTML structure for a single tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newt",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SrIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants!"
+  },
+  "created_at": 1461116232227
+}
+
+$(document).ready(function () {
+
+  const createTweetElement = function (tweetObject) {
+    const $oneTweet = $(`
+      <article>
+      <div class="tweets-user-container">
+      <div class="tweet-user">
+        <img src="${tweetObject.user.avatars}" alt="cartoon headshot user avatar">
+        <h2>${tweetObject.user.name}</h2>
+      </div>
+
+      <p class="tweet-username">${tweetObject.user.handle}</p>
+      </div>
+
+      <p class="tweet-content ">
+      ${tweetObject.content.text}
+      </p>
+
+      <footer>
+      <p>${tweetObject.created_at}</p>
+
+      <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+
+      </div>
+      </footer>
+      </article>`
+    );
+
+    return $oneTweet
+  }
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+}); // END document ready
