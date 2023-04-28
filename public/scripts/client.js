778eac3e9055f238429d98c19453cd7f0f6730a0
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-27 21:49:45 -0400

    update data object to contain multiple users, for looping purposes in the next function implementation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,17 +5,31 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newt",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SrIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants!"
-  },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 
 $(document).ready(function () {
 
